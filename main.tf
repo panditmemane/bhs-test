4d75f10c973d1f3ace74f0a8f38c0455a8e66aed
panditmemane <44285326+panditmemane@users.noreply.github.com>
2022-07-31 15:00:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ resource "random_pet" "rg-name" {
 resource "azurerm_resource_group" "rg" {
   name      = random_pet.rg-name.id
   location  = var.resource_group_location
-
 }
 
 # Create virtual network
@@ -88,8 +87,6 @@ resource "azurerm_storage_account" "mystorageaccount" {
   name                     = "diag${random_id.randomId.hex}"
   location                 = azurerm_resource_group.rg.location
   resource_group_name      = azurerm_resource_group.rg.name
-  container_name           = "tfstatefiles"
-  key                      = "dev.terrafrom.tfstate"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
