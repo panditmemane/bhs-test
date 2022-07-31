32d15600fc1cd570a94b750007c4c235fe0962c4
panditmemane <44285326+panditmemane@users.noreply.github.com>
2022-07-31 15:18:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,9 @@ resource "azurerm_virtual_network" "myterraformnetwork" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  storage_account_name = "terrafromcodes"
+  container_name       = "tfstatefiles"
+  key                  = "dev.terrafrom.tfstate"
 }
 
 # Create subnet
