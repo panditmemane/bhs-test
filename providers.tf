78fc0b69595d08d7451dcbe921d40ee7f5d87384
panditmemane <44285326+panditmemane@users.noreply.github.com>
2022-07-31 14:27:38 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,15 +1,9 @@
-terraform {
-
-  required_version = ">=0.12"
-
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
-
 provider "azurerm" {
-  features {}
+    version = "=1.38.0"
+    #version = "=1.44.0"
+    subscription_id = "d8001963-4d58-4b89-a56f-3db63f96cbe6"
+    client_id       = "09f68dcb-3b89-49d6-becd-452b2bcfa41a"
+    client_secret   = "e49e67e3-8629-43c7-8204-2602c7d59b36"
+    tenant_id       = "4c6daf9f-820a-4a2a-99c5-1e27564c397e"
+    #skip_provider_registration = "true"
 }
