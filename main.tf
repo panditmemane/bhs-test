21814017a0590a1ed4ddd7dbfb2995292423acc2
panditmemane <44285326+panditmemane@users.noreply.github.com>
2022-07-31 15:12:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -89,6 +89,7 @@ resource "azurerm_storage_account" "mystorageaccount" {
   resource_group_name      = azurerm_resource_group.rg.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  
 }
 
 # Create (and display) an SSH key
