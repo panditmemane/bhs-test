ed4c990d63d0e2c36544beb39525e19e70e249a2
panditmemane <44285326+panditmemane@users.noreply.github.com>
2022-07-31 16:18:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 
-  backend "azurerm" {
-    resource_group_name  = "TerraformDemo"
-    storage_account_name = "terraformcodeops"
-    container_name       = "tfstatefile"
-    key                  = "dev.terraform.tfstate"
-  }
+ #backend "azurerm" {
+  #  resource_group_name  = "TerraformDemo"
+    #storage_account_name = "terraformcodeops"
+  #  container_name       = "tfstatefile"
+ #   key                  = "dev.terraform.tfstate"
+ #}
 module "RG" {
   source   = "./modules/RG" #A
   rgname   = var.rgname     #B
