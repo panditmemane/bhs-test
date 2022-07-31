7c9b4a7699d734585bb98226be6e7f128362c3cf
panditmemane <44285326+panditmemane@users.noreply.github.com>
2022-07-31 14:35:24 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -13,7 +13,7 @@ terraform {
 provider "azurerm" {
     subscription_id = "d8001963-4d58-4b89-a56f-3db63f96cbe6"
     client_id       = "09f68dcb-3b89-49d6-becd-452b2bcfa41a"
-    client_secret   = "e49e67e3-8629-43c7-8204-2602c7d59b36"
+    client_secret   = "JE78Q~~wQxEDguXLElZL2d-53Ynk7NxaLjOPgdd_"
     tenant_id       = "4c6daf9f-820a-4a2a-99c5-1e27564c397e"
   features {}
 }
