016aab12dc053004f0c1eb5b7e85458355e1c884
panditmemane <44285326+panditmemane@users.noreply.github.com>
2022-07-31 19:55:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,8 +46,8 @@ resource "azurerm_network_security_group" "myterraformnsg" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    #destination_port_range     = "22"
-    destination_port_range     = "*"
+    destination_port_range     = "22"
+    #destination_port_range     = "*"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
