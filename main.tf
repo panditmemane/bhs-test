114dbdd92722a3c79439a107524e6b594fb69811
panditmemane <44285326+panditmemane@users.noreply.github.com>
2022-07-31 16:28:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,4 @@
 
-resource "azurerm" {
-    resource_group_name  = "TerraformDemo"
-    storage_account_name = "terraformcodeops"
-    container_name       = "tfstatefile"
-    key                  = "dev.terraform.tfstate"
-  }
-
-module "RG" {
-  source   = "./modules/RG" #A
-  rgname   = var.rgname     #B
-  location = var.location
-}
-
-############################
 resource "random_pet" "rg-name" {
   prefix    = var.resource_group_name_prefix
 }
