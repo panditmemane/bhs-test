17a13ff4d9e08de01c530f52213af0c65024bd49
panditmemane <44285326+panditmemane@users.noreply.github.com>
2022-08-02 15:18:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,10 @@ terraform {
 }
 
 provider "azurerm" {
+    subscription_id = "d8001963-4d58-4b89-a56f-3db63f96cbe6"
+    client_id       = "09f68dcb-3b89-49d6-becd-452b2bcfa41a"
+    client_secret   = "JE78Q~~wQxEDguXLElZL2d-53Ynk7NxaLjOPgdd_"
+    tenant_id       = "4c6daf9f-820a-4a2a-99c5-1e27564c397e"
   features {}
 }
 
